8bbfe21967d79b1e658005e940cc0b86b6f986af
Ashutosh singh <81563630+ashutoshsinghcs2@users.noreply.github.com>
2025-06-05 13:23:13 +0530

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -65,35 +66,69 @@ namespace Bulk_Test_Vexil.Controllers
             return View();
         }
 
+        //private void SaveStudentAndDocuments(Student student)
+        //{
+        //    using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        //    {
+        //        conn.Open();
+
+        //        // Insert student
+        //        string insertStudentQuery = "INSERT INTO Students (Name, MobileNo) VALUES (@Name, @MobileNo); SELECT SCOPE_IDENTITY();";
+        //        using (SqlCommand cmd = new SqlCommand(insertStudentQuery, conn))
+        //        {
+        //            cmd.Parameters.AddWithValue("@Name", student.Name);
+        //            cmd.Parameters.AddWithValue("@MobileNo", student.MobileNo);
+        //            student.StudentId = Convert.ToInt32(cmd.ExecuteScalar());
+        //        }
+
+        //        // Insert documents
+        //        foreach (var document in student.StudentDocumentsList)
+        //        {
+        //            string insertDocumentQuery = "INSERT INTO StudentDocuments (StudentId, DocumentType, FilePath) VALUES (@StudentId, @DocumentType, @FilePath)";
+        //            using (SqlCommand cmd = new SqlCommand(insertDocumentQuery, conn))
+        //            {
+        //                cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
+        //                cmd.Parameters.AddWithValue("@DocumentType", document.DocumentType);
+        //                cmd.Parameters.AddWithValue("@FilePath", document.FilePath);
+        //                cmd.ExecuteNonQuery();
+        //            }
+        //        }
+        //    }
+        //}
+
         private void SaveStudentAndDocuments(Student student)
         {
             using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
             {
                 conn.Open();
 
-                // Insert student
-                string insertStudentQuery = "INSERT INTO Students (Name, MobileNo) VALUES (@Name, @MobileNo); SELECT SCOPE_IDENTITY();";
-                using (SqlCommand cmd = new SqlCommand(insertStudentQuery, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Name", student.Name);
-                    cmd.Parameters.AddWithValue("@MobileNo", student.MobileNo);
-                    student.StudentId = Convert.ToInt32(cmd.ExecuteScalar());
-                }
+                // Create a DataTable to hold the documents
+                DataTable documentsTable = new DataTable();
+                documentsTable.Columns.Add("DocumentType", typeof(string));
+                documentsTable.Columns.Add("FilePath", typeof(string));
 
-                // Insert documents
                 foreach (var document in student.StudentDocumentsList)
                 {
-                    string insertDocumentQuery = "INSERT INTO StudentDocuments (StudentId, DocumentType, FilePath) VALUES (@StudentId, @DocumentType, @FilePath)";
-                    using (SqlCommand cmd = new SqlCommand(insertDocumentQuery, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
-                        cmd.Parameters.AddWithValue("@DocumentType", document.DocumentType);
-                        cmd.Parameters.AddWithValue("@FilePath", document.FilePath);
-                        cmd.ExecuteNonQuery();
-                    }
+                    documentsTable.Rows.Add(document.DocumentType, document.FilePath);
+                }
+
+                // Define the SqlCommand and parameters
+                using (SqlCommand cmd = new SqlCommand("dbo.SaveStudentAndDocuments", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@Name", student.Name);
+                    cmd.Parameters.AddWithValue("@MobileNo", student.MobileNo);
+
+                    SqlParameter tvpParam = cmd.Parameters.AddWithValue("@Documents", documentsTable);
+                    tvpParam.SqlDbType = SqlDbType.Structured;
+                    tvpParam.TypeName = "dbo.StudentDocumentType";
+
+                    cmd.ExecuteNonQuery();
                 }
             }
         }
+
 
     }
 }
