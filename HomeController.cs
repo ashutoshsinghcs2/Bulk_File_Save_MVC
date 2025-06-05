e01344ae723a12e8ab2012bf34a0737c0bd6aa7c
Ashutosh singh <81563630+ashutoshsinghcs2@users.noreply.github.com>
2025-06-05 13:00:02 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using Bulk_Test_Vexil.Models;
+
+namespace Bulk_Test_Vexil.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            var studentDocuments = new List<StudentDocuments>();
+
+            foreach (string fileKey in Request.Files)
+            {
+                string pattern = @"\[(\d+)\]"; // Regex to match digits inside square brackets
+
+                Match match = Regex.Match(fileKey, pattern);
+
+                var postedFile = Request.Files[fileKey] as HttpPostedFileBase;
+                if (postedFile != null && postedFile.ContentLength > 0)
+                {
+                    // Generate a unique file name
+                    string fileName = Path.GetFileName(postedFile.FileName);
+                    string filePath = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+
+                    // Ensure the directory exists
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                    // Save the file to the server
+                    postedFile.SaveAs(filePath);
+
+                    // Create a new StudentDocuments object
+                    var document = new StudentDocuments
+                    {
+                        DocumentType = Request.Form["StudentDocumentsList"+ match + ".DocumentType"],
+                        FilePath = filePath
+                    };
+                    studentDocuments.Add(document);
+                }
+            }
+
+            student.StudentDocumentsList = studentDocuments;
+
+            // Save student and documents to the database
+            SaveStudentAndDocuments(student);
+
+            return View();
+        }
+
+        private void SaveStudentAndDocuments(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                conn.Open();
+
+                // Insert student
+                string insertStudentQuery = "INSERT INTO Students (Name, MobileNo) VALUES (@Name, @MobileNo); SELECT SCOPE_IDENTITY();";
+                using (SqlCommand cmd = new SqlCommand(insertStudentQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", student.Name);
+                    cmd.Parameters.AddWithValue("@MobileNo", student.MobileNo);
+                    student.StudentId = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+
+                // Insert documents
+                foreach (var document in student.StudentDocumentsList)
+                {
+                    string insertDocumentQuery = "INSERT INTO StudentDocuments (StudentId, DocumentType, FilePath) VALUES (@StudentId, @DocumentType, @FilePath)";
+                    using (SqlCommand cmd = new SqlCommand(insertDocumentQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
+                        cmd.Parameters.AddWithValue("@DocumentType", document.DocumentType);
+                        cmd.Parameters.AddWithValue("@FilePath", document.FilePath);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bulk_Test_Vexil.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+        public List<StudentDocuments> StudentDocumentsList { get; set; }
+    }
+
+    public class StudentDocuments
+    {
+        public int DocumentId { get; set; }
+        public string DocumentType { get; set; }
+        public HttpPostedFileBase Document { get; set; }
+        public string FilePath { get; set; }
+    }
+
+}
